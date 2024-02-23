2bc72bb3c8d720c5812f8cbe8f32bddbc0eda56f
SyedAhsan24 <syedahsan94040@gmail.com>
2024-02-23 13:22:50 +0500

    practice in js

---
+++ app.js
@@ -0,0 +1,107 @@
+// chapter no 1
+// Q no 1
+ alert("hello world")
+// // Q no 2
+ alert ("hello ahsan")
+// // Q no 3
+ alert( "welcome to js land.. \n Happy coding!")
+// // Q no 4
+ alert ("Welcome to js land..")
+ alert("Happy coding \n prevent this page form coding additional dialogs.")
+// // Q no 5
+ alert("Hello..I can run js through my web browser's console")
+// Q no 6
+// This is working html&css
+//Q no 7 
+// This is working abcd  
+// chapter no 1 is complete
+
+// chapter no 2
+// Q no 1
+ alert("syed ahsan")
+// // Q no 2
+ alert("syed ahsan nawab")
+// // Q no3
+var message ="hello world"
+alert(message)
+// // Q no 4
+var name = "syed ahsan"
+var age = "18"
+var qulification ="certified mobile application dvelopment"
+alert(name)
+alert(age)
+alert(qulification)
+// // Q no 5
+ alert("pizza \n Pizz \n Piz \n PI \n P")
+// // Q no 6
+var email = "example@example.com"
+alert("my email address is"+" " +" " +email)
+// // Q no 7
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the book"+" "+ book)
+// // Q no 8
+ document.write("<b>Yah! I can write HTML content through javascript</b>")
+// chapter no complete
+
+// chapter no 3
+// Q no 1
+ var age ="15"
+ alert("I am " +age+" years old")
+// Q no 2
+// skip this question
+// // Q no 3
+var birthyear="2005"
+document.write("my birth year is" + birthyear + "<br> data type declared variable is number")  
+// Q no 4
+// skip this question
+
+// chapter no 1
+var firstname ="syed"
+var lastname ="ahsan"
+var email ="syedahsan@gmail.com"
+var phone ="03102861161"
+var password ="lahore"
+alert(firstname)
+alert(lastname)
+alert(email)
+alert(phone)
+alert(password)
+// // Q no 2
+ alert("you are learning javascript")
+// // Q no 3
+alert ("forget the work of the world and focus on the work of god. Here everything is sucessful")
+
+// chapter no 2
+// Q no 1
+var  same = "quintity"  
+// Q no 2
+var casequintity
+casequintity = 30
+// Q no 3
+var teamname ="quetta gladiators"
+alert(teamname)
+// Q no 4
+var bestMan ="Charlie"
+bestMan ="anas"
+// chapter no 2 complete
+
+// chapter no 3
+// Q no 1
+var caseQty 
+// Q no 2
+var caseQty =144
+// Q no 3
+var number =9;
+// Q no 4
+var number= 10
+var number2 =20
+alert(10 + number2)
+// Q no 5
+var marchtotal=100;
+var shippingcharge=10;
+var ordertotal=marchtotal+shippingcharge;
+console.log(ordertotal);
+// Q no 6
+var mynumber=10;
+mynumber=mynumber+5;
+// chapter no 3 complete
